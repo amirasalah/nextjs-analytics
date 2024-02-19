01e106b6c125fc47ee5ff268930c8449a7aa2662
amirasalah <amera.salah.89@gmail.com>
2024-02-19 16:17:49 +0200

    adding analytics

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,20 +1,26 @@
 import {redis} from '../lib/redis';
 
 type AnalyticsArgs = {
-    retention?: number;
+    retention?: number
+}
+
+type TrackOptions = {
+    persist?: boolean
 }
 
 export class Analytics {
     private retention:number = 7 * 24 * 60 * 60;
 
     constructor(options?: AnalyticsArgs) {
-        if(options?.retention) {
-            this.retention = options.retention;
-        }
+        if(options?.retention) this.retention = options.retention;
     }
 
-    async track(namespace:string, event: object = {}) {
-        const key  = `analytics::${namespace}`;
+    async track(namespace:string, event: object = {}, options?: TrackOptions) {
+        let key  = `analytics::${namespace}`;
+
+        if(!options?.persist){
+            key+= `::${Date.now()}`;
+        }
 
         await redis.hincrby(key, JSON.stringify(event), 1);
     }
